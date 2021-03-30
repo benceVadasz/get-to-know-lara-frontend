69eac9c38052225c4db652adf251fc3779f03cf0
vadaszbence <64799151+vadaszbence@users.noreply.github.com>
2021-03-30 18:35:53 +0200

    add styling to header

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,7 +3,6 @@ import {Link} from "react-router-dom";
 import {makeStyles} from "@material-ui/core/styles";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
-import Typography from "@material-ui/core/Typography";
 import Button from "@material-ui/core/Button";
 import axios from "axios";
 import SendIcon from "@material-ui/icons/Send";
@@ -11,6 +10,29 @@ import IconButton from "@material-ui/core/IconButton";
 import Spinner from "react-spinner-material";
 
 export default function Navbar() {
+    const useStyles = makeStyles((theme) => ({
+        load: {
+            position: "fixed", top: "50%", left: "50%", transform: "translate(-50%, -50%)"
+        },
+        root: {
+            background: "#F6F6F6",
+            flexGrow: 1,
+            marginBottom: "5px",
+        },
+        menuButton: {
+            marginRight: theme.spacing(2),
+        },
+        title: {
+            flexGrow: 1,
+        },
+        navbarStyle: {
+            background: "#003366",
+        },
+        img: {
+            width: 150,
+            marginTop: 5,
+        },
+    }));
 
     const isLoggedIn = sessionStorage.getItem("email") != null;
     let navRow;
