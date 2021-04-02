39638e9fa16b286fe77ce213afd25056eb5e4645
vadaszbence <64799151+vadaszbence@users.noreply.github.com>
2021-04-02 12:15:00 +0200

    add base mail table to inbox

--- src/components/InboxContent.js
+++ src/components/InboxContent.js
@@ -3,6 +3,7 @@ import {makeStyles} from "@material-ui/core/styles";
 import axios from 'axios';
 import Typography from "@material-ui/core/Typography";
 import {BASE_URL} from '../constants';
+import {Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow} from "@material-ui/core";
 
 const drawerWidth = 240;
 const useStyles = makeStyles((theme) => ({
@@ -44,7 +45,7 @@ function InboxHeader() {
     const [loading, setLoading] = useState(false);
 
     useEffect(() => {
-        setLoading(true);
+        setMails([{'name': 'Ben', 'sent': '2010'}, {'name': 'Bob', 'sent': '2012'}]);
         axios
             .get(`${BASE_URL}/inbox`, {
                 headers: {
@@ -54,7 +55,8 @@ function InboxHeader() {
             .then((res) => {
                 setLoading(false);
                 console.log(res.data);
-                setMails(res.data.mail);
+                // setMails(res.data.mail);
+
             });
         // eslint-disable-next-line
     }, []);
@@ -78,7 +80,40 @@ function InboxHeader() {
     return (
         <main className={classes.content}>
             <div className={classes.toolbar}/>
-            <p>poop</p>
+            <TableContainer component={Paper}>
+                <Table className={classes.table} aria-label="simple table">
+                    <TableHead>
+                        <TableRow>
+                            <TableCell>Subject</TableCell>
+                            <TableCell align="right">From</TableCell>
+                            <TableCell align="right">Sent at</TableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {mails.map((mail, i) => (
+                            <TableRow key={mail.id}>
+                                {/*<TableCell className={classes.linkTitle}>*/}
+                                {/*    {!mail.is_read ? (*/}
+                                {/*        <i id="dot" class="fa fa-circle" aria-hidden="true"></i>*/}
+                                {/*    ) : (*/}
+                                {/*        ""*/}
+                                {/*    )}*/}
+                                {/*    <Link*/}
+                                {/*        to={"view/" + mail.mailId}*/}
+                                {/*        // onClick={openMail(mail.id)}*/}
+                                {/*        className={classes.mailLink}*/}
+                                {/*        align="left"*/}
+                                {/*    >*/}
+                                {/*        {mail.subject}*/}
+                                {/*    </Link>*/}
+                                {/*</TableCell>*/}
+                                <TableCell align="right">{mail.name}</TableCell>
+                                <TableCell align="right">{mail.sent}</TableCell>
+                            </TableRow>
+                        ))}
+                    </TableBody>
+                </Table>
+            </TableContainer>
         </main>
     );
 }
