7afbe6a4f502daadd7ce80c820c6a787169903a1
vadaszbence <64799151+vadaszbence@users.noreply.github.com>
2021-04-02 11:40:22 +0200

    add useEffect to inbox content

--- src/components/InboxContent.js
+++ src/components/InboxContent.js
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import {makeStyles} from "@material-ui/core/styles";
+import axios from 'axios';
 
 const drawerWidth = 240;
 const useStyles = makeStyles((theme) => ({
@@ -27,11 +28,30 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 function InboxHeader() {
+    const token = sessionStorage.getItem("token");
     const classes = useStyles();
     const [mobileOpen, setMobileOpen] = React.useState(false);
+    const [mails, setMails] = useState([]);
+    const baseUrl =
+        "http://localhost:8888/get-to-know-lara/get-to-know-lara-backend/public/api/inbox";
+    const [loading, setLoading] = useState(false);
     const handleDrawerToggle = () => {
         setMobileOpen(!mobileOpen);
     };
+    useEffect(() => {
+        setLoading(true);
+        axios
+            .get(baseUrl, {
+                headers: {
+                    Authorization: "Bearer " + token,
+                },
+            })
+            .then((res) => {
+                setLoading(false);
+                console.log(res.data);
+                setMails(res.data.mail);
+            });
+    }, []);
     return (
         <main className={classes.content}>
             <div className={classes.toolbar} />
